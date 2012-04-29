d08c3211052789ec2a2bb15499f39921610ad387
Kyle Tolle <kyle@nullsix.com>
2012-04-29 16:57:39 -0600

    Accepts only 2 fighters.

--- textfight_server.rb
+++ textfight_server.rb
@@ -1,10 +1,31 @@
 require 'socket'
 
+def broadcast(text, fighters)
+  fighters.each do |fighter|
+    fighter.puts text
+  end
+end
+
+
 s = TCPServer.new(3939)
+fighters = []
 
 while (fighter = s.accept)
   Thread.new(fighter) do |f|
-    f.puts "Hello, fighter!"
+    if fighters.size >= 2
+      f.puts "There are already 2 fighters!"
+      f.close
+    end
+
+    fighters << f
+    broadcast("New fighter connected!", fighters)
+
+    f.puts "What's your name?"
+    while f.gets
+      # Do something
+    end
+
+    fighters.delete f
     f.close
   end
 end
